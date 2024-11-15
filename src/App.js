81b32ee8012437ab8cf492a0a2e5b344857ac693
itsmeeSanj <itsmeesanj@gmail.com>
2024-11-15 13:32:17 -0500

    add:currency covertor

--- src/App.js
+++ src/App.js
@@ -3,26 +3,60 @@ import React from "react";
 // https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD
 
 function App() {
-  const [inputVal, setInputVal] = React.useState("");
-  const [fromCur, setFromCur] = React.useState("");
-  const [toCur, setToCur] = React.useState("");
+  const [inputVal, setInputVal] = React.useState("1");
+  const [InputCur, setInputCur] = React.useState("USD");
+  const [outpuCur, setOutputCur] = React.useState("CAD");
 
   const [curData, setCurData] = React.useState({});
+  const [error, setError] = React.useState("");
 
   React.useEffect(
     function () {
+      const controller = new AbortController();
+      const signal = controller.signal;
+
       async function CurConvertApi() {
-        const Api = await fetch(
-          `https://api.frankfurter.app/latest?amount=${inputVal}&from=${fromCur}&to=${toCur}`
-        );
-        const data = await Api.json();
-        console.log("data", data);
-        setCurData(data);
+        try {
+          setError("");
+
+          const Api = await fetch(
+            `https://api.frankfurter.app/latest?amount=${Number(
+              inputVal
+            )}&from=${InputCur}&to=${outpuCur}`,
+            { signal }
+          );
+
+          if (!Api?.ok) {
+            throw new Error("Somethig went wrong while fetching movies!");
+          }
+          const data = await Api.json();
+          console.log("data", data);
+          setCurData(data);
+        } catch (error) {
+          if (error.name !== "AbortError") {
+            setError(error.message);
+          }
+          setError(error.message);
+        }
       }
       CurConvertApi();
     },
-    [inputVal, fromCur, toCur]
+    [inputVal, InputCur, outpuCur]
   );
+
+  function outputResult(output, data) {
+    switch (output) {
+      case "USD":
+        return data?.USD;
+      case "EUR":
+        return data?.EUR;
+      case "INR":
+        return data?.INR;
+
+      default:
+        return data?.CAD;
+    }
+  }
 
   return (
     <div>
@@ -31,19 +65,23 @@ function App() {
         value={inputVal}
         onChange={(e) => setInputVal(Number(e.target.value))}
       />
-      <select value={fromCur} onChange={(e) => setFromCur(e.target.value)}>
+      <select value={InputCur} onChange={(e) => setInputCur(e.target.value)}>
         <option value='USD'>USD</option>
         <option value='EUR'>EUR</option>
         <option value='CAD'>CAD</option>
         <option value='INR'>INR</option>
       </select>
-      <select value={toCur} onChange={(e) => setToCur(e.target.value)}>
+      <select value={outpuCur} onChange={(e) => setOutputCur(e.target.value)}>
         <option value='USD'>USD</option>
         <option value='EUR'>EUR</option>
         <option value='CAD'>CAD</option>
         <option value='INR'>INR</option>
       </select>
-      <p>OUTPUT </p>
+      <p>
+        {InputCur === outpuCur
+          ? inputVal
+          : outputResult(outpuCur, curData?.rates)}
+      </p>
     </div>
   );
 }
